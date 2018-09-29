6f8478393e777a75dff1c2dcec321a8248d2ef9d
Bryant Deters <bryantdeters@gmail.com>
2018-09-28 23:08:32 -0500

    using regex to better parse html response

--- src/main.rs
+++ src/main.rs
@@ -1,81 +1,47 @@
 extern crate reqwest;
-
+extern crate regex;
 use std::fs;
 use std::io::Write;
 use std::thread;
-use time::Duration;
+use regex::Regex;
 
 fn main() {
-    //TODO: random thread::sleep offset on each child thread to bypass the site's rate limits
     //TODO: finish formatting of response to a useful csv
     let west_weekday_handle = thread::spawn(|| {
-        thread::sleep(Duration::from_secs())
-        match schedule_request("https://www.metrostlouis.org/wp-admin/admin-ajax.php?action=metro_build_metrolink_html_table&direction=west&day_type=weekdays"){
-        Ok(s) => {
-            let csv = filter_content(s);
-            save_to_csv(csv, "westbound-weekday-schedule.csv").unwrap();
-        }
-        Err(_) => {
-            writeln!(std::io::stderr(), "error retrieving westbound weekday schedule");
-        }
-    }
+        generate_schedule(
+            "https://www.metrostlouis.org/wp-admin/admin-ajax.php?action=metro_build_metrolink_html_table&direction=west&day_type=weekdays",
+            "westbound-weekday-schedule.csv");
     });
-
     let east_weekday_handle = thread::spawn(|| {
-        match schedule_request("https://www.metrostlouis.org/wp-admin/admin-ajax.php?action=metro_build_metrolink_html_table&direction=east&day_type=weekdays"){
-            Ok(s) => {
-                let csv = filter_content(s);
-                save_to_csv(csv, "westbound-weekday-schedule.csv").unwrap();
-            }
-            Err(_) => {
-                writeln!(std::io::stderr(), "error retrieving westbound weekday schedule");
-            }
-        }
+        generate_schedule(
+            "https://www.metrostlouis.org/wp-admin/admin-ajax.php?action=metro_build_metrolink_html_table&direction=east&day_type=weekdays",
+            "eastbound-weekday-schedule.csv"
+        );
     });
 
     let west_saturday_handle = thread::spawn(|| {
-        match schedule_request("https://www.metrostlouis.org/wp-admin/admin-ajax.php?action=metro_build_metrolink_html_table&direction=west&day_type=saturday"){
-            Ok(s) => {
-                let csv = filter_content(s);
-                save_to_csv(csv, "westbound-saturday-schedule.csv").unwrap();
-            }
-            Err(_) => {
-                writeln!(std::io::stderr(), "error retrieving westbound saturday schedule");
-            }
-        }
+        generate_schedule(
+            "https://www.metrostlouis.org/wp-admin/admin-ajax.php?action=metro_build_metrolink_html_table&direction=west&day_type=saturday",
+            "westbound-saturday-schedule.csv"
+        );
     });
     let east_saturday_handle = thread::spawn(|| {
-        match schedule_request("https://www.metrostlouis.org/wp-admin/admin-ajax.php?action=metro_build_metrolink_html_table&direction=east&day_type=saturday"){
-            Ok(s) => {
-                let csv = filter_content(s);
-                save_to_csv(csv, "westbound-saturday-schedule.csv").unwrap();
-            }
-            Err(_) => {
-                writeln!(std::io::stderr(), "error retrieving westbound saturday schedule");
-            }
-        }
+        generate_schedule(
+            "https://www.metrostlouis.org/wp-admin/admin-ajax.php?action=metro_build_metrolink_html_table&direction=east&day_type=saturday",
+            "eastbound-saturday-schedule.csv"
+        );
     });
     let west_sunday_handle = thread::spawn(|| {
-        match schedule_request("https://www.metrostlouis.org/wp-admin/admin-ajax.php?action=metro_build_metrolink_html_table&direction=west&day_type=sunday"){
-            Ok(s) => {
-                let csv = filter_content(s);
-                save_to_csv(csv, "westbound-sunday-schedule.csv").unwrap();
-            }
-            Err(_) => {
-                writeln!(std::io::stderr(), "error retrieving westbound sunday schedule");
-            }
-        }
+        generate_schedule(
+            "https://www.metrostlouis.org/wp-admin/admin-ajax.php?action=metro_build_metrolink_html_table&direction=west&day_type=sunday",
+            "westbound-sunday-schedule.csv"
+        );
     });
     let east_sunday_handle = thread::spawn(|| {
-        match schedule_request("https://www.metrostlouis.org/wp-admin/admin-ajax.php?action=metro_build_metrolink_html_table&direction=east&day_type=sunday"){
-            Ok(s) => {
-                let csv = filter_content(s);
-                save_to_csv(csv, "eastbound-sunday-schedule.csv").unwrap();
-            }
-            Err(_) => {
-                writeln!(std::io::stderr(), "error retrieving eastbound sunday schedule");
-            }
-        }
+        generate_schedule(
+            "https://www.metrostlouis.org/wp-admin/admin-ajax.php?action=metro_build_metrolink_html_table&direction=east&day_type=sunday",
+            "eastbound-sunday-schedule.csv"
+        );
     });
 
     west_weekday_handle.join().unwrap();
@@ -94,88 +60,43 @@ fn schedule_request(url: &str) -> Result<String, reqwest::Error> {
 
 //edit string or return clone? depends what the string helpers do
 fn filter_content(junk: String) -> String {
-    return junk.replace("{\"type\":\"success\",\"html\":", "") //remove start and end lines
+    let junk = junk.replace("{\"type\":\"success\",\"html\":", "") //remove start and end lines
         .replace("}", "")
+        .replace(r#"<\/thead>"#, "\n")
+        .replace(r#"<\/tr>"#, "\n")
+        .replace(r#"<\/td>"#, ",");
+    let re = Regex::new(r#"<[\w|\s|\d|=|"|\-|\\|/]*>"#).unwrap();
+    let s = re.replace_all(&junk, "");
+    return s
+        .replace("\\n\\t\\t\\t\\t", "")
+        .replace("\\n\\t\\t\\t", "")
+        .replace("\\n\\t\\t", ",")
         .replace(" pm", "P") //convert am/pm to A/P
         .replace(" am", "A")
-        .replace("metro", "") //remove html class words
-        .replace("undefined", "")
-        .replace("blue", "")
-        .replace("line", "")
-        .replace("background", "")
-        .replace("red", "")
-        .replace("bold", "")
-        .replace("indicator", "")
-        .replace("schedule", "")
-        .replace("system", "")
-        .replace("table", "")
-        .replace("link", "")
-        .replace("-td", "")
-        .replace("span", "") //remove html tag names
-        .replace("class=", "")
-        .replace("thead", "")
-        .replace("time", "")
-        .replace("/", "") //remove particles
-//        .replace("\t", "")
-        .replace("\\t", "")
-        .replace("\n", "")
-        .replace("\\n", "")
-        .replace("\\", "")
-        .replace("\"", "")
-        .replace("<", "")
-        .replace(">", "")
-        .replace("tr", "\n")
         .replace("-", "")
-        .replace("td", ",") //comma-separate
-        .replace("  ", "") //truncate spaces
-        .replace(", ", ",")
-        .replace("d,", ",")
-        .replace(",d", ","); //trim whitespace
+        .replace("\\t", "")
+        .replace("\\n", "")
+        .replace(",,\n,", ",\n")
+        .replace("\"", "")
+        .replacen(",", "", 1)
+        .replacen("\n", "", 1);
 }
 
 //write to filesystem
 fn save_to_csv(contents: String, name: &str) -> std::io::Result<()> {
-    let mut f = fs::File::create(format!("../out/{}", name))?;
+    let mut f = fs::File::create(format!("./out/{}", name))?;
     f.write_all(contents.as_bytes())?;
     Ok(())
 }
 
-//    let contents = fs::read_to_string("sample-data/html-response.txt").expect("something went wrong")
-//        .replace("{\"type\":\"success\",\"html\":", "") //remove start and end lines
-//        .replace("}", "")
-//        .replace(" pm", "P") //convert am/pm to A/P
-//        .replace(" am", "A")
-//        .replace("metro", "") //remove html class words
-//        .replace("undefined", "")
-//        .replace("blue", "")
-//        .replace("line", "")
-//        .replace("background", "")
-//        .replace("red", "")
-//        .replace("bold", "")
-//        .replace("indicator", "")
-//        .replace("schedule", "")
-//        .replace("system", "")
-//        .replace("table", "")
-//        .replace("link", "")
-//        .replace("-td", "")
-//        .replace("span", "") //remove html tag names
-//        .replace("class=", "")
-//        .replace("thead", "")
-//        .replace("time", "")
-//        .replace("/", "") //remove particles
-////        .replace("\t", "")
-//        .replace("\\t", "")
-//        .replace("\n", "")
-//        .replace("\\n", "")
-//        .replace("\\", "")
-//        .replace("\"", "")
-//        .replace("<", "")
-//        .replace(">", "")
-//        .replace("tr", "\n")
-//        .replace("-", "")
-//        .replace("td", ",") //comma-separate
-//        .replace("  ", "") //truncate spaces
-//        .replace(", ", ","); //trim whitespace
-//
-//
-//    println!("{:?}", contents);
+fn generate_schedule(url: &str, filename: &str) {
+    match schedule_request(url) {
+        Ok(s) => {
+            let csv = filter_content(s);
+            save_to_csv(csv, filename).unwrap();
+        }
+        Err(_) => {
+            writeln!(std::io::stderr(), "error retrieving schedule from url");
+        }
+    }
+}
